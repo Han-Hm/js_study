b3e272fe571146911c700eb4baee29f728cc09b1
Han-Hm <hyemihan89@gmail.com>
2020-04-26 00:22:35 +0900

    30-Day LeetCoding Challenge: day 9

--- leetCode/last_stone_weight.js
+++ leetCode/last_stone_weight.js
@@ -1,11 +1,23 @@
 /**
+ * We have a collection of stones, each stone has a positive integer weight.
+ * Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
  * @param {number[]} stones
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    const sortedStone = stones.sort((a, b) => b - a));
-    let temp = sortedStone[0] - sortedStone[1];
-    for (let i = 1; i < sortedStone.length - 1; i++) {
-        sortedStone
+    let sortedStone = stones.sort((a, b) => b - a);
+    while (sortedStone.length > 1) {
+        let temp = sortedStone[0] - sortedStone[1];
+        sortedStone.splice(0, 2);
+        if (temp !== 0) {
+            sortedStone.push(temp);
+            sortedStone = sortedStone.sort((a, b) => b - a);
+        }
     }
+    
+    return sortedStone[0] || 0;
 };
+
+const assert = require('assert');
+assert.equal(lastStoneWeight([2,7,4,1,8,1]), 1);
+assert.equal(lastStoneWeight([2,2]), 0);
